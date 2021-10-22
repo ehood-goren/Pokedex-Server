787232be961f0d5303903d3d7994abcb16427b3e
EhoodGoren <ehoodgoren@gmail.com>
2021-10-23 00:41:50 +0300

    Sending a post request will return the user's username

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 9000;
 const pokemonRouter = require('./src/routers/pokemonRouter');
+const userRouter = require('./src/routers/userRouter');
 
 app.listen(port, () => {
     console.log('listening...');
 });
 
 app.use('/pokemon', pokemonRouter);
+app.use('/info', userRouter);
