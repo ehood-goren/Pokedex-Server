95fcf9484f572c320cb75dcc85372356f9a6bbd8
DanielPhilosoph <danielph9@gmail.com>
2021-10-24 01:08:35 +0300

    Done

--- src/middleware/userHandler.js
+++ src/middleware/userHandler.js
@@ -1,10 +1,19 @@
-function userHandler(req, res, next){
-    const username = req.headers.username;
-    if(!username){
-        throw Error('401');
+const fs = require("fs");
+function userHandler(req, res, next) {
+  const username = req.headers.username;
+  try {
+    let users = fs.readFileSync("./users");
+    let index = users.indexOf(username);
+    if (index === -1) {
+      throw Error("401");
+    } else {
+      next();
+      return;
     }
+  } catch (error) {
     next();
+    throw Error("401");
+  }
 }
-
 
 module.exports = userHandler;
